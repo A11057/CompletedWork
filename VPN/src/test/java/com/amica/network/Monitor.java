c1105ac4e6a2704635249a4d54f4450cdbe20528
Sherry Custodio <scustodio@amica.com>
2022-02-16 14:02:20 -0500

    more vpn changes

--- VPN/src/test/java/com/amica/network/Monitor.java
+++ VPN/src/test/java/com/amica/network/Monitor.java
@@ -1,7 +1,14 @@
 package com.amica.network;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Monitor {
+
+    @Scheduled(fixedRate = 5000)
+    public void schedule() {
+
+
+    }
 }
--- VPN/src/test/java/com/amica/network/VPN.java
+++ VPN/src/test/java/com/amica/network/VPN.java
@@ -9,11 +9,17 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 public class VPN {
 
+    public enum Status {RUNNING, STOPPED, STARTING}
+
     public static void main(String[] args) {
 
         System.setProperty("management.endpoints.web.exposure.include", "*");
         System.setProperty("management.endpoint.shutdown.enabled", "true");
         System.setProperty("endpoints.shutdown.enabled", "true");
+
+        System.setProperty("logging.level.com.amica.network.Monitor", "INFO");
+
+        System.setProperty("env.name", "developerworkstation");
 
         SpringApplication.run(VPN.class, args);
     }
